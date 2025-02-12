4f2d603ba1c8b9f109c30a2df1cbd4175b925fdb
NourElden <emohmednour@gmail.com>
2025-02-12 19:59:40 +0200

    Add project files.

---
+++ IdentityManager/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace IdentityManager
+{
+    public class ApplicationDbContext: IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options ): base(options)
+        {
+            
+        }
+    }
+}
